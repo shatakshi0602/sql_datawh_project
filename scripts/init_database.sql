deb9242aeb6057e7c4ad6e80a1aade6380bed84e
shatakshi0602 <shatakshisingh6302@gmail.com>
2026-02-22 20:03:13 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,8 @@
-
+use master;
+-- created database for our data warehousing project 
+create database datawh_2026;
+use datawh_2026;
+-- creating schema for medallion data warehouse architecture 
+create schema bronze;-- No transformations as is data storage layer 
+create schema gold -- Transformations but no data modelling layer
+create schema silver;-- Implementing business logic here 
